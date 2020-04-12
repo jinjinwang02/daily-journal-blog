cc8d8f1830874d9f11dd67283e8f765dfbb73e6b
Jinjin Wang <jinjinwang@MacBook-Pro.local>
2020-04-12 16:43:16 +0100

    Edit about page and merge about page and contact page

--- app.js
+++ app.js
@@ -77,9 +77,6 @@ app.get("/about", function(req, res) {
   res.render("about");
 });
 
-app.get("/contact", function(req, res) {
-  res.render("contact");
-});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
