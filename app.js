34230c608faeede651e19cc58820a14423407b02
Jinjin Wang <jinjinwang@MacBook-Pro.local>
2020-04-12 17:22:32 +0100

    Add Procfile and update port

--- app.js
+++ app.js
@@ -78,7 +78,11 @@ app.get("/about", function(req, res) {
   res.render("about");
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server has started successfully.");
 });
